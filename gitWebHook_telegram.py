c6e67660c9f55380d598e860cd9a06f74e1a1b0d
Felipe Odorizi <sfelipeodorizi@gmail.com>
2026-02-19 11:33:51 +0000

    Add shell and git webhook with kafka

--- gitWebHook_telegram.py
+++ gitWebHook_telegram.py
@@ -9,6 +9,7 @@ bot = telebot.TeleBot("7576015296:AAHPoWA5p6WPrYdcwk_MXXB-EWaN2SBPlLA")
 chat_id = -5290134232
 
 
+
 def send_telegram_message(message):
     """Envia mensagem Telegram"""
     bot.send_message(chat_id, message)
